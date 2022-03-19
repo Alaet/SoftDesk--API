9381e2fdc46bf9c9c3d1ee7b0713292441237da9
Alaet <72195619+Alaet@users.noreply.github.com>
2022-03-19 13:05:30 +0100

    Update admin.py
    added project object to admin site for UI managment of DB

--- APISoftDesk/authentication/admin.py
+++ APISoftDesk/authentication/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from authentication.models import User
+from project.models import Project, Issue, Comment, Contributors
 
 admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Issue)
+admin.site.register(Comment)
+admin.site.register(Contributors)
