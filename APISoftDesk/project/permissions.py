c9913fc11425f44fe8f17223a2d487f8c2f0e7c3
Alaet <72195619+Alaet@users.noreply.github.com>
2022-03-19 11:52:42 +0100

    Update permissions.py
    fiefract on contributor permissions

--- APISoftDesk/project/permissions.py
+++ APISoftDesk/project/permissions.py
@@ -27,6 +27,7 @@ class IsOwner(BasePermission):
         :param obj:
         :return: bool
         """
+        # if isinstance(obj, Issue) or isinstance()
         if type(obj) == Issue or type(obj) == Comment:
             if request.user.id == obj.author_user_id.id:
                 return True
@@ -57,11 +58,9 @@ class IsContributor(BasePermission):
             return True
 
     def has_object_permission(self, request, view, obj):
-        if type(obj) == Issue or type(obj) == Contributors:
+        if type(obj) == Issue:
             contrib = Contributors.objects.filter(project_id=obj.project_id.id).values_list('user_id', flat=True)
-            if request.user.id == contrib[0]:
-                return True
-            elif request.user.id in contrib:
+            if request.user.id in contrib:
                 return True
         elif type(obj) == Project:
             contrib = Contributors.objects.filter(project_id=obj.id).values_list('user_id', flat=True)
@@ -71,4 +70,8 @@ class IsContributor(BasePermission):
             issue = get_object_or_404(Issue, pk=obj.issue_id.id)
             contrib = Contributors.objects.filter(project_id=issue.project_id).values_list('user_id', flat=True)
             if request.user.id in contrib:
+                return True
+        elif type(obj) == Contributors:
+            contrib = Contributors.objects.filter(project_id=obj.project_id.id).values_list('user_id', flat=True)
+            if request.user.id == contrib[0]:
                 return True
