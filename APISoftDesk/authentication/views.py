1336c455020cc5d2d62be08073ad4231459172d7
Alaet <72195619+Alaet@users.noreply.github.com>
2022-03-19 14:47:31 +0100

    Update views.py
    refract + fix get_permussuibs

--- APISoftDesk/authentication/views.py
+++ APISoftDesk/authentication/views.py
@@ -1,19 +1,24 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from authentication.permissions import SignupPermissions
+from authentication.models import User
 from authentication.serializers import UserCreation as s_UserCreation
-from project.models import USER_MODEL
 
 
 class UserCreation(ModelViewSet):
 
     serializer_class = s_UserCreation
-    permission_classes = [IsAuthenticated, SignupPermissions]
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method == "GET":
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
 
     def get_queryset(self):
         """
         Get list fo every User in DB
         :return: All DB  ->  User(AbstractUser)
         """
-        return USER_MODEL.objects.all()
+        return User.objects.all()
